7bd58f582343edffa252ff26dcaeba4abc4f6fcc
Armando Cerna <armando@cerna.ca>
2019-04-27 16:20:16 -0700

    add gauge for space used

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module nzbpaws
 
 go 1.12
 
-require github.com/gorilla/rpc v1.2.0
+require (
+	github.com/gizak/termui v2.3.0+incompatible // indirect
+	github.com/gizak/termui/v3 v3.0.0 // indirect
+	github.com/gorilla/rpc v1.2.0
+	github.com/maruel/panicparse v1.1.1 // indirect
+	github.com/mattn/go-runewidth v0.0.4 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.0 // indirect
+	github.com/nsf/termbox-go v0.0.0-20190325093121-288510b9734e // indirect
+)
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
 	"github.com/gorilla/rpc/json"
 	"log"
 	"net/http"
@@ -13,15 +15,16 @@ import (
 )
 
 var (
-	nzbGetUser string = os.Getenv("NZBGET_USER")
-	nzbGetPass string = os.Getenv("NZBGET_PASS")
-	pauseThreshold = flag.Uint64("pause-threshold", 10, "pause threshold in GB" )
-	unpauseThreshold = flag.Uint64("unpause-threshold", 50, "unpause threshold in GB" )
-	ssl = flag.Bool("ssl", false, "Use SSL for communication with nzbget" )
-	hostname = flag.String("host", "localhost", "hostname for nzbget" )
-	port = flag.String("port", "6789", "port for nzbget" )
-	dir = flag.String("dir", "/", "directory to check" )
-	paused bool
+	nzbGetUser       string = os.Getenv("NZBGET_USER")
+	nzbGetPass       string = os.Getenv("NZBGET_PASS")
+	pauseThreshold          = flag.Uint64("pause-threshold", 10, "pause threshold in GB")
+	unpauseThreshold        = flag.Uint64("unpause-threshold", 50, "unpause threshold in GB")
+	ssl                     = flag.Bool("ssl", false, "Use SSL for communication with nzbget")
+	hostname                = flag.String("host", "localhost", "hostname for nzbget")
+	port                    = flag.String("port", "6789", "port for nzbget")
+	dir                     = flag.String("dir", "/", "directory to check")
+	paused           bool
+	resp             bool
 )
 
 func main() {
@@ -32,46 +35,74 @@ func main() {
 	if len(nzbGetPass) == 0 {
 		log.Fatalf("missing env NZBGET_USER\n")
 	}
+
+	if err := ui.Init(); err != nil {
+		log.Fatalf("failed to initlize termui %v", err)
+	}
+
+	// Create Gauge
+	g0 := widgets.NewGauge()
+	g0.SetRect(20, 20, 130, 30)
+
+	uiEvents := ui.PollEvents()
+
 	for {
-		var resp bool
-		s, err := getDiskSpace(*dir)
+		e := <-uiEvents
+		switch e.ID {
+		case "q", "<C-c>":
+			return
+		}
+
+		s, t, err := getDiskSpace(*dir)
 		if err != nil {
 			log.Fatalf("Failed to determine disk space: %v\n", err)
 		}
+
+		percentUsed := int((float64(s) / float64(t)) * 100)
+		g0.Percent = int(percentUsed)
 		if s < (*pauseThreshold) && !paused {
-			fmt.Printf("Pausing Downloads: current free disk space is: %v GB. Threshold to pause (%v GB) reached.\n", s, *pauseThreshold)
+			g0.Label = fmt.Sprintf("Pausing Downloads: current free disk space is: %v GB. Threshold to pause (%v GB) reached.\n",
+				s, *pauseThreshold)
 			if resp, err = nzbGet("pausedownload"); err != nil {
 				log.Fatalf("Error Pausing Downloads: %v\n", err)
 			}
+			g0.BarColor = ui.ColorRed
 			paused = true
 			fmt.Println(resp)
 		} else if s > (*unpauseThreshold) && paused {
-			fmt.Printf("Resuming downloads: current free disk space is: %v GB. Threshold to unpause (%v GB) reached.\n", s, *unpauseThreshold)
+			g0.Label = fmt.Sprintf("Resuming downloads: current free disk space is: %v GB. Threshold to unpause (%v GB) reached.\n",
+				s, *unpauseThreshold)
 			if resp, err = nzbGet("resumedownload"); err != nil {
 				log.Fatalf("Error Pausing Downloads: %v\n", err)
 			}
+			g0.BarColor = ui.ColorGreen
 			paused = false
 			fmt.Println(resp)
 		}
 		if paused {
-			fmt.Printf("Downloads Paused, current free disk space is: %v GB. Threshold to unpause (%v GB) has not been reached.\n", s, *unpauseThreshold)
+			g0.Label = fmt.Sprintf("Downloads Paused, current free disk space is: %v GB. Threshold to unpause (%v GB) has not been reached.\n",
+				s, *unpauseThreshold)
+			g0.BarColor = ui.ColorRed
 		} else {
-			fmt.Printf("Nothing to see here, current free disk space is: %v GB. Threshold to pause (%v GB) has not been reached.\n", s, *pauseThreshold)
+			g0.Label = fmt.Sprintf("Nothing to see here, current free disk space is: %v GB. Threshold to pause (%v GB) has not been reached.\n",
+				s, *pauseThreshold)
+			g0.BarColor = ui.ColorGreen
 		}
+		ui.Render(g0)
 		time.Sleep(time.Second * 60)
 	}
 
 }
 
-func getDiskSpace(path string) (uint64, error) {
+func getDiskSpace(path string) (uint64, uint64, error) {
 	var stat syscall.Statfs_t
 
 	err := syscall.Statfs(path, &stat)
 	if err != nil {
-		return 0, err
+		return 0, 0, err
 	}
 
-	return (stat.Bavail * uint64(stat.Bsize)) / 1024 / 1024 / 1024, nil
+	return (stat.Bavail * uint64(stat.Bsize)) / 1024 / 1024 / 1024, (stat.Blocks * uint64(stat.Bsize)) / 1024 / 1024 / 1024, nil
 }
 
 func nzbGet(action string) (bool, error) {
@@ -100,7 +131,7 @@ func nzbGet(action string) (bool, error) {
 		return false, fmt.Errorf("error sending request: %v", err)
 	}
 
-	defer func () {
+	defer func() {
 		err := resp.Body.Close()
 		if err != nil {
 			log.Fatalf("Error closing response body %v", err)
